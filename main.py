5ee0881935ed3e0b407ec8c11c9cf79d1be999e3
Denis <byd.mak@gmail.com>
2021-10-29 18:09:55 +0300

    0.03

--- main.py
+++ main.py
@@ -1,50 +1,6 @@
-import openpyxl
 import re
 
-excel_file = openpyxl.load_workbook('Table.xlsx')
-
-# print(excel_file.sheetnames) # ['TDSheet'] - получение название листа
-employees_sheet = excel_file['TDSheet']
-currently_active_sheet = excel_file.active
-
-maxRow = employees_sheet.max_row
-maxColumn = employees_sheet.max_column
-listName = list()
-strCode = list()
-twoStolbesh = list()
-
-for i in range(maxRow - 9):
-    i = i + 9
-    i = str(i)
-    m = employees_sheet["A" + i].value
-    strCode.append(str(m))  # - заполняем первый столбец
-
-for i in range(maxRow - 9):
-    i = i + 9
-    i = str(i)
-    m = employees_sheet["C" + i].value
-    twoStolbesh.append(str(m))
-    if str(i) == str(maxRow-1):
-        for j in range(6):
-            twoStolbesh.append("del")
-
-for k in range(len(strCode)):
-    listName.append(("list" + str(k)))  # создаем массив всех элементов
-    listName[k] = [1, 2, 3, 4, 5]
-f = 0
-for j in range(len(strCode)):
-    if not re.search(r'\d{2}.\d{2}\D{3}', strCode[j]) is None:  # делаем нужную выборку
-        listName[f][0] = strCode[j]
-        listName[f][1] = employees_sheet["A" + str(j + 11)].value
-        listName[f][2] = employees_sheet["A" + str(j + 12)].value
-        listName[f][3] = employees_sheet["A" + str(j + 13)].value
-        print(strCode[j])
-        print(twoStolbesh[j+4])
-
-        f = f + 1
-
-for i, e in reversed(list(enumerate(listName))):
-    if str(listName[i][0]) == '1':
-        del listName[i]
-print(twoStolbesh)
-print(listName)
+string = '30'
+b = string.split()
+b = ''.join(b)
+print(int(b))
--- test.py
+++ test.py
@@ -2,18 +2,24 @@ import openpyxl
 import json
 import re
 
-excel_file = openpyxl.load_workbook('Table2.xlsx')
+excel_file = openpyxl.load_workbook('Tb1.xlsx')
 
-# print(excel_file.sheetnames) # ['TDSheet'] - получение название листа
-employees_sheet = excel_file['TDSheet']
+namelist = excel_file.sheetnames     # ['TDSheet'] - получение название листа
+employees_sheet = excel_file[str(namelist[0])]
 currently_active_sheet = excel_file.active
 
 maxRow = employees_sheet.max_row
 maxColumn = employees_sheet.max_column
-maxposition = 0
 listName = list()
 strCode = list()
 twoStolbesh = list()
+f = 0
+collposit = list()
+collp = 0
+shagi1 = 0
+shagi2 = 0
+shagi = 10
+shetchik = 10
 
 for i in range(maxRow - 9):
     i = i + 10
@@ -27,7 +33,6 @@ for k in range(len(twoStolbesh)):
     listName.append(("list" + str(k)))  # создаем массив всех элементов
     listName[k] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-f = 0
 for j in range(len(twoStolbesh)):
     if str(twoStolbesh[j]) != 'None':
         listName[f][0] = str(f + 1)
@@ -46,29 +51,7 @@ for j in range(len(twoStolbesh)):
 for i, e in reversed(list(enumerate(listName))):
     if str(listName[i][5]) == '5':
         del listName[i]
-# for k in range(len(listName)):
-#     NameStolb = [
-#         "ID",
-#         "Категория",
-#         "Номер",
-#         "ФИО ответственного",
-#         "Отдел",
-#         "Основное средство",
-#         "Инвентарный номер",
-#         "ОКОФ",
-#         "Амортизационная группа",
-#         "Способ начисления амортизации",
-#         "Дата ввода в эксплуатацию",
-#         "Состояние",
-#         "Срок полезного использования",
-#         "Мес. норма износа,",
-#         "Износ"]
-#     fruit_dictionary = dict(zip(NameStolb, listName[k]))
-#     with open("data_file.json", "a") as write_file:
-#         json.dump(fruit_dictionary, write_file, ensure_ascii=False, indent=2)
 
-collposit = list()
-collp = 0
 for m in range(len(strCode)):
     k = str(strCode[m])
     if not re.search(r'\d{2}.\d{2}\D{3}', strCode[m]) is None:
@@ -77,19 +60,47 @@ for m in range(len(strCode)):
         collp = 0
     else:
         collp = collp + 1
-del collposit[0]
 
-shagi = 11
-shetchik = 0
-for i in range(1, len(collposit), 2):
-    shagi = shagi + collposit[i] - 1
-    shagi1 = employees_sheet["A" + str(shagi)].value
-    #print(shagi1)
-    print(collposit[shetchik])
-    shetchik = shetchik + 2
+# Добавляем в номенклатуры 10.34(эти номера)
+for i in range(2, len(collposit), 2):
+    shetchik = shetchik + int(collposit[i])
+    strdelprobel = str(employees_sheet["A" + str(shetchik)].value)
+    delprobel = strdelprobel.split()
+    delprobel = ''.join(delprobel)
+    shagi1 = int(delprobel) + int(shagi1)
+    shagi1 = shagi1 - shagi2
+    shetchik = int(shetchik) + 1
+    for j in range(shagi2, shagi1):
+        listName[j][1] = collposit[i-1]
+    shagi2 = shagi1
 
+# Добавляем в последние номенклатуры 10.34(эти номера)
+lastposishion = len(listName)
+for k in range(shagi2, lastposishion):
+    listName[k][1] = collposit[-1]
 
+print(listName)
+# От сюда нужно получить сначала 101.32 или типо того, затем получить количество номенклатур входящих в неё, далее такому количеству по порядку изменить первое поле на это название.
+# С отделом и ФИО: Берем ячейку с номенклатурой и берем ячейку слева выше и проверяем ее на число, если это не число то записываем к этой номеклатуре эту клетку и над ней клетку, если число то идем выше, пока не найдем не число, если над не числом находится число, то номенклатуре добавляем прочерк.
 
-#print(strCode)
-print(collposit)
-#print(listName)
+# Запись в json
+for k in range(len(listName)):
+    NameStolb = [
+        "ID",
+        "Категория",
+        "Номер",
+        "ФИО ответственного",
+        "Отдел",
+        "Основное средство",
+        "Инвентарный номер",
+        "ОКОФ",
+        "Амортизационная группа",
+        "Способ начисления амортизации",
+        "Дата ввода в эксплуатацию",
+        "Состояние",
+        "Срок полезного использования",
+        "Мес. норма износа,",
+        "Износ"]
+    fruit_dictionary = dict(zip(NameStolb, listName))
+    with open("data_file.json", "a") as write_file:
+        json.dump(fruit_dictionary, write_file, ensure_ascii=False, indent=2)
